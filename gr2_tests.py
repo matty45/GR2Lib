ac7102d53b23b92ca4fb3c477978c84333344e45
Matthew <matthewtoo5@googlemail.com>
2024-08-09 20:33:00 +0100

    move testing related stuff to its own dir

--- gr2_tests.py
+++ gr2_tests.py
@@ -1,7 +1,7 @@
 """This triggers all the tests to make sure things work correctly."""
 
-from gr2_size_test import check_granny_struct_sizes
-from load_gr2_test import load_gr2_test
+from tests.gr2_size_test import check_granny_struct_sizes
+from tests.load_gr2_test import load_gr2_test
 
 # Make sure we implemented the structs correctly or so help me god.
 check_granny_struct_sizes()
